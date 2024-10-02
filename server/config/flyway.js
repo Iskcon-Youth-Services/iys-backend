22a35b551cd32fe85d5150730609906c18dffdb7
SubhamPanda2003 <sajalkumarpanda@gmail.com>
2024-10-02 23:33:35 +0530

    some files updated

--- server/config/flyway.js
+++ server/config/flyway.js
@@ -1,11 +1,24 @@
+// require('dotenv').config();
+
+// module.exports = {
+//     flywayArgs: {
+//     url:  process.env.DB_URL,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     schemas: ['public'],
+//     locations: ['filesystem:./migrations']
+//     }
+// };
+
+
 require('dotenv').config();
 
 module.exports = {
     flywayArgs: {
-    url:  process.env.DB_URL,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    schemas: ['public'],
-    locations: ['filesystem:./migrations']
+        url: `jdbc:postgresql://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        schemas: ['public'],
+        locations: ['filesystem:./migrations']
     }
 };
