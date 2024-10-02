168659d2e2d2eedbf414b591838af282f958e242
SubhamPanda2003 <sajalkumarpanda@gmail.com>
2024-10-02 23:43:24 +0530

    Update V4__Add_session_table.sql

--- migrations/V4__Add_session_table.sql
+++ migrations/V4__Add_session_table.sql
@@ -6,10 +6,10 @@ CREATE TABLE IF NOT EXISTS user_sessions (
         );
 
 
-CREATE TABLE role (
-    user_id INT PRIMARY KEY,
-    role ENUM('devotee', 'center_admin', 'super_admin') NOT NULL DEFAULT 'devotee',
-    CONSTRAINT unique_super_admin CHECK (
-        role != 'super_admin' OR (SELECT COUNT(*) FROM role WHERE role = 'super_admin') < 1
-    )
-);
+-- CREATE TABLE role (
+--     user_id INT PRIMARY KEY,
+--     role ENUM('devotee', 'center_admin', 'super_admin') NOT NULL DEFAULT 'devotee',
+--     CONSTRAINT unique_super_admin CHECK (
+--         role != 'super_admin' OR (SELECT COUNT(*) FROM role WHERE role = 'super_admin') < 1
+--     )
+-- );
